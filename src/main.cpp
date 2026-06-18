fcd53060b11bfc87f1278eaa273abba527ecb574
Kuba-Dvorak <jakub.dvorak.025@pslib.cz>
2026-06-18 21:32:41 +0200

    local
    # Conflicts:
    #	platformio.ini
    #	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 // v commandu jsem pouzil to ze kdyz je treba G1 tak to je 1, G21 je 21, jasne?
 struct basicCMD {
     uint8_t command;
-    float x, y, z;
     float speed;
 };
 
@@ -15,15 +14,13 @@ struct calibration {
 
 
 struct toolheadInfo {
-    float x, y, z;
-    uint8_t stopXfront, stopYfront, stopZfront, stopXend, stopYend, stopZend;
 };
 
 
 void setup() {
-// write your initialization code here
+    // write your initialization code here
 }
 
 void loop() {
-// write your code here
+    // write your code here
 }
